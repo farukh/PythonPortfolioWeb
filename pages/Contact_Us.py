05fb0a4e1fc499163fb324cbd411d04cc8aa368a
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-12 22:54:18 +0500

    - Created Email Form for Contact Us page

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,2 +1,9 @@
 import streamlit as st
 st.title("Contact Us")
+with st.form(key="email_form"):
+    user_email = st.text_input("Your Email Address:")
+    message = st.text_area("Your Message: ")
+    btn_submit = st.form_submit_button("Submit")
+    print(btn_submit)
+    if btn_submit:
+        print('Button Pressed...')
