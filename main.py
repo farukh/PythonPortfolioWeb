7298ce86ead7c42568f70e73b58d66b2b048edc8
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-12 18:26:26 +0500

    Day 21 - Added Portfolio description

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
+
 with col1:
     st.image('images/photo.jpeg')
 
@@ -12,3 +13,5 @@ with col2:
     My career as professional include over 16 years of management and hands-on development experience. Had worked using variety of technologies, tools, languages and architectures. Have introduced successful products at different stages of jobs. Have developed skills, learnt new technologies and adopted best practices which helped me to provide efficient and reliable results.
     """
     st.info(content)
+content = """Below you can find some of app that I have develop in Python. Feel free to contact me."""
+st.write(content)
